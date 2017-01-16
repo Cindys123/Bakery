a6c947615b1372c9b3fde86715f58d1dfd07207f
Cindys123 <reymart.delara@aol.com>
2017-01-16 17:10:14 +0800

    Add files via upload

--- Login.php
+++ Login.php
@@ -1,1 +1,25 @@
+<?php
+$con = mysqli_connect("localhost","id553952_cindysbakery","09085139083","id553952_cindysbakery");
 
+$number = $_POST["number"];
+$email = $_POST["email"];
+
+$statement = mysqli_prepare($con,"SELECT * from user where number = ? and email = ?");
+mysqli_stmt_bind_param($statement, "ss", $number, $email);
+mysqli_stmt_execute($statement);
+
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($statement, $user_id, $name, $number, $email, $address);
+
+$response = array();
+$response["success"] = false;
+
+while(mysqli_stmt_fetch($statement)){
+	$response[""] = true;
+	$response["name"] = $name;
+	$response["number"] = $number;
+	$response["address"] = $address;
+	$response["email"] = $email;
+}
+echo json_decode($response);
+?>
--- Register.php
+++ Register.php
@@ -1,1 +1,21 @@
+<?php
+$con = mysqli_connect("localhost","id553952_cindysbakery","09085139083","id553952_cindysbakery");
 
+
+$name = $_POST["name"];
+$address = $_POST["address"];
+$number = $_POST["number"];
+$email = $_POST["email"];
+
+$statement = mysqli_prepare($con,"INSERT INTO user (name, address,email,number) VALUES(?,?,?,?)");
+mysqli_stmt_bind_param($statement, "siss", $name, $address, $number, $email);
+mysqli_stmt_execute($statement);
+
+mysqli_stmt_store_result($statement);
+mysqli_stmt_bind_result($statement, $user_id, $name, $number, $email, $address);
+
+$response = array();
+$response["success"] = true;
+
+echo json_decode($response);
+?>
