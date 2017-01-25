2a5d51334dbaa864c4eba03cf78f0d7d65fb3d56
Cindys123 <reymart.delara@aol.com>
2017-01-26 00:14:53 +0800

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,15 +1,15 @@
 <?php
-$con = mysqli_connect("localhost","id553952_cindysbakery","09085139083","id553952_cindysbakery");
+$con = mysqli_connect("cindysbakery.000webhostapp.com","id553952_cindysbakery","09085139083","id553952_loginregistercindys");
 
-$number = $_POST["number"];
 $email = $_POST["email"];
+$password = $_POST["password"];
 
-$statement = mysqli_prepare($con,"SELECT * from user where number = ? and email = ?");
-mysqli_stmt_bind_param($statement, "ss", $number, $email);
+$statement = mysqli_prepare($con,"SELECT * from cindy where email = ? and password = ?");
+mysqli_stmt_bind_param($statement, "ss", $email, $password);
 mysqli_stmt_execute($statement);
 
 mysqli_stmt_store_result($statement);
-mysqli_stmt_bind_result($statement, $user_id, $name, $number, $email, $address);
+mysqli_stmt_bind_result($statement, $user_id, $name, $password,$email, $birthday, $mgender,$fgender);
 
 $response = array();
 $response["success"] = false;
@@ -17,9 +17,11 @@ $response["success"] = false;
 while(mysqli_stmt_fetch($statement)){
 	$response[""] = true;
 	$response["name"] = $name;
-	$response["number"] = $number;
-	$response["address"] = $address;
+	$response["password"] = $password;
 	$response["email"] = $email;
+	$response["birthday"] = $birthday;
+	$response["mgender"] = $mgender;
+	$response["fgender"] = $fgender;
 }
 echo json_decode($response);
 ?>
